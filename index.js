6fe3023218a26ff54a06a94cb9cc7bd1ff0f282a
bmw78 <bmw78@byu.edu>
2018-10-19 03:55:34 +0000

    Removal and Hover text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 angular.module('app', [])
   .controller('mainCtrl', mainCtrl)
   .directive('fc', fcDirective);
-  
+
 function mainCtrl($scope) {
 
   $scope.cards = [];
@@ -9,43 +9,54 @@ function mainCtrl($scope) {
   $scope.addNew = function(card) {
     $scope.cards.push({
       front: card.front,
-      back:card.back
+      back: card.back
     }); /* [1] */
 
     console.log($scope.cards)
-    card.front = ''; 
-    card.back = ''; 
-    
+    card.front = '';
+    card.back = '';
+
     $scope.countCards();
   };
-  
-  $scope.setSelectedCard = function (card) {
-        $scope.selectedCard = card;
-    };
+
+  $scope.setSelectedCard = function(card) {
+    $scope.selectedCard = card;
+  };
 
   $scope.countCards = function() {
     document.getElementById("numberCards").innerHTML = $scope.cards.length;
   }
+
+  $scope.remove = function() {
+    var oldList = $scope.cards;
+    $scope.cards = [];
+    angular.forEach(oldList, function(card) {
+      if (!card.done) $scope.cards.push(card);
+    });
+  };
+  
+
 }
 
 
 function fcDirective() {
   return {
     scope: {
-      card: '=' 
+      card: '='
     },
     restrict: 'E',
     replace: 'true',
     template: (
-      '<div class="Flashcard">' +
-      '<h4>{{card.front}}</h4>' + '<h5>{{card.back}}</h5>'
-      + '</div>'
+      '<div class="Flashcard" title="{{card.back}}">' +
+      '<input type="checkbox"  ng-model="card.done">' +
+      '<span> {{card.front}}</span>' // + '<h5>{{card.back}}</h5>'
+      +
+      '</div>'
     ),
     link: link
   };
 
-  function link(scope)
-  {
-      console.log(scope.cards);
+  function link(scope) {
+    console.log(scope.cards);
   }
 }
